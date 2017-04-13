29cf03c516b19a2563d714b1974e9ff7c7ec3eec
Ryan Cain <ryan.cain@precisionplanting.com>
2017-04-13 14:19:37 -0500

    Updates

--- docker/setup_docker.sh
+++ docker/setup_docker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # SETUP ENV
-COREOS_IPXE_SERVER_BASE_URL="<ip/dns record>:4777"
+COREOS_IPXE_SERVER_BASE_URL="pp-rancher01.prp.local:4777"
 COREOS_IPXE_SERVER_LISTEN_ADDR="0.0.0.0:4777"
-VERSIONS=("367.1.0" "379.3.0")
+VERSIONS=("current")
 
 # PREPARE DIRECTORY STRUCTURE
 mkdir -p {configs,images,profiles,sshkeys}
@@ -13,55 +13,14 @@ for VERSION in "${VERSIONS[@]}"
 do
 	echo "Downloading files for version ${VERSION}"
 	mkdir -p images/amd64-usr/$VERSION
-	wget -nc http://storage.core-os.net/coreos/amd64-usr/$VERSION/coreos_production_pxe_image.cpio.gz -O images/amd64-usr/${VERSION}/coreos_production_pxe_image.cpio.gz
-	wget -nc http://storage.core-os.net/coreos/amd64-usr/$VERSION/coreos_production_pxe.vmlinuz -O images/amd64-usr/${VERSION}/coreos_production_pxe.vmlinuz
+	wget -nc http://alpha.release.core-os.net/amd64-usr/$VERSION/coreos_production_pxe_image.cpio.gz -O images/amd64-usr/${VERSION}/coreos_production_pxe_image.cpio.gz
+	wget -nc http://alpha.release.core-os.net/amd64-usr/$VERSION/coreos_production_pxe.vmlinuz -O images/amd64-usr/${VERSION}/coreos_production_pxe.vmlinuz
 done
 
 # CUSTOMIZE COREOS SERVER CONFIGURATION
-echo "<ssh-key>" > sshkeys/coreos.pub
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC4tBuQACn8seokY88d1IA8+e2UOSjqVwYtpkGOG8xdG4rMm5DkibssVE0ksm/vY3FrK6ByyfPxs7lmnzmCIGQDlTulEriSoajo091fPh2pIGAtYxVeyknO5gFdYcy1Gb1eXlsArYhgmaiGNMk2IvzXNNVlzZZmSH5eZympttnUIJF+0I+J/PgIbaAiMf8430nFDYLXRRqyx1lPJInf0DcDysc1mlIQyfeazv9VsZFalTZL0hzAjmrqPZU+rBpbsM4cz8U/4wE8Pjyvl1i1x0DAQP8Dyo9OoZB7LT5h7n+syICetylO32hjHp4LI9h+/UG9OeHGO6+Dpe1kVmTGif5R pp-admin" > sshkeys/coreos.pub
 
-
-cat > configs/development.yml <<EOF
-#cloud-config
-
-ssh_authorized_keys:
-  - <ssh-key>
-
-coreos:
-  etcd:
-      # generate a new token for each unique cluster from https://discovery.etcd.io/new
-      # WARNING: replace each time you 'vagrant destroy'
-      discovery: https://discovery.etcd.io/a1efed8239a47c98c12ce07e2b67f0ed
-      addr: $public_ipv4:4001
-      peer-addr: $public_ipv4:7001
-  units:
-    - name: etcd.service
-      command: start
-    - name: fleet.service
-      command: start
-      runtime: no
-      content: |
-        [Unit]
-        Description=fleet
-
-        [Service]
-        Environment=FLEET_PUBLIC_IP=$public_ipv4
-        ExecStart=/usr/bin/fleet
-    - name: docker-tcp.socket
-      command: start
-      enable: true
-      content: |
-        [Unit]
-        Description=Docker Socket for the API
-
-        [Socket]
-        ListenStream=2375
-        Service=docker.service
-        BindIPv6Only=both
-
-        [Install]
-        WantedBy=sockets.target
-EOF
+wget -nc https://git.ppdev.io/snippets/134/raw -O configs/development.yml
 
 for VERSION in "${VERSIONS[@]}"
 do
@@ -76,8 +35,8 @@ EOF
 done
 
 # BUILD DOCKER
-docker build -t jveverka/ipxe .
+docker build -t norova/coreos-ipxe-server .
 
 # RUN DOCKER
-docker run -d -p 4777:4777 -e "COREOS_IPXE_SERVER_BASE_URL=${COREOS_IPXE_SERVER_BASE_URL}" -e "COREOS_IPXE_SERVER_LISTEN_ADDR=${COREOS_IPXE_SERVER_LISTEN_ADDR}" jveverka/ipxe
+docker run -d -p 4777:4777 -e "COREOS_IPXE_SERVER_BASE_URL=${COREOS_IPXE_SERVER_BASE_URL}" -e "COREOS_IPXE_SERVER_LISTEN_ADDR=${COREOS_IPXE_SERVER_LISTEN_ADDR}" norova/coreos-ipxe-server
 
